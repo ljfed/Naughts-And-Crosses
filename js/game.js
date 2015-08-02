477afeac512371cb7ece7e5ff6fb4a13033eaa16
ljfed <laurence_fed@hotmail.com>
2015-08-02 14:54:12 +1000

    added working reset button

--- js/game.js
+++ js/game.js
@@ -4,18 +4,20 @@ $(document).ready(function(){
         if (counter%2 === 0) {
             $(this).attr('src', 'images/x.png');
             counter = counter+1;
-            console.log(counter);
+            //console.log(counter);
         } else if (counter === 9) {
             
         } else {
             $(this).attr('src', 'images/circle.png');
             counter = counter+1;
-            console.log(counter);            
+            //console.log(counter);            
         }
     });
 });
-function isEven(n){
-    checker = n%2;
-    console.log(checker);
-}
-isEven(8);
+
+$(document).ready(function(){
+    $('#reset').click(function(){
+        $('.gameTile').attr('src', 'images/blank.png');
+        counter = 0;
+    });
+});
