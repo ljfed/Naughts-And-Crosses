9c0b1e76c115452c0bd1e38bdd6369f35dbd7d53
ljfed <laurence_fed@hotmail.com>
2015-08-03 20:52:02 +1000

    Fixed bug where you can click on the same tile twice

--- js/game.js
+++ js/game.js
@@ -1,23 +1,45 @@
 var counter = 0;
+var tilesClicked = [];
+
+function isObjInArr(obj, arr) {
+    for (var i=0;i<arr.length;i++) {
+        if (arr[i] === obj) {
+            return true; //is in the array
+        }
+    }
+    return false;
+}
+
+//Tiles change on click:
 $(document).ready(function(){
     $('.gameTile').click(function() {
-        if (counter%2 === 0) {
-            $(this).attr('src', 'images/x.png');
-            counter = counter+1;
-            //console.log(counter);
-        } else if (counter === 9) {
-            
+        tileClicked = $(this).attr('id');
+        if (counter%2 === 0) { //checking counter is even
+            if (isObjInArr(tileClicked, tilesClicked)) { //checking if tile has been clicked
+                //Do nothing (function returns true)
+            } else { //tile hasn't been clicked       
+                $(this).attr('src', 'images/x.png');
+                tilesClicked.push(tileClicked);
+                counter++;
+            }            
         } else {
-            $(this).attr('src', 'images/circle.png');
-            counter = counter+1;
-            //console.log(counter);            
+            if (isObjInArr(tileClicked, tilesClicked)) {
+                //do nothing (function returns true)
+            } else {
+                $(this).attr('src', 'images/circle.png');
+                tilesClicked.push(tileClicked);
+                counter++;
+            }
         }
     });
 });
 
+
+//Reset Button:
 $(document).ready(function(){
     $('#reset').click(function(){
         $('.gameTile').attr('src', 'images/blank.png');
         counter = 0;
+        tilesClicked = [];
     });
 });
